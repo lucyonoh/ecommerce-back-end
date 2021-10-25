c372ada2f22d134013dbd02a0c72c1776cdb94f1
Lucy Onoh'git config --global user.name Lucy <lanenza26@gmail.com>
2021-10-25 11:46:41 -0500

    updating js

---
+++ models/ProductTag.js
@@ -0,0 +1,40 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection');
+
+class ProductTag extends Model {}
+
+ProductTag.init(
+  {
+    // define columns
+    id:{
+      type:DataTypes.INTEGER,
+      allowNull:false,
+      primaryKey:true,
+      autoIncrement:true
+    },
+    product_id:{
+      type:DataTypes.INTEGER,
+      references:{
+        model:'product',
+        key:'id'
+      }//foreign key
+    },
+    tag_id:{
+      type:DataTypes.INTEGER,
+      references:{
+        model:'tag',
+        key:'id'
+      }//foreign key
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'productTag',
+  }
+);
+
+module.exports = ProductTag;
